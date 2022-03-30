c931d9fa257d2ed1a8a2b7d397c830e12c17e725
Jakub Pravda <jakub.pravda@comsource.cz>
2022-03-30 13:56:05 +0200

    curl techlib.cz

--- src/main.go
+++ src/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
-	"golang.org/x/net/html"
+	"fmt"
+	"io/ioutil"
 	"log"
-	"strings"
+	"net/http"
 	"strconv"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 const currentOccupancyText = "Current\u00A0number of\u00A0persons in the library"
+const techlibEnIndex = "https://www.techlib.cz/en/"
 
 func extractCurrentNumberOfPersons(techlibIndex string) int {
 	log.Println("Extracting current occupancy from techlib index page")
@@ -31,13 +36,13 @@ func extractCurrentNumberOfPersons(techlibIndex string) int {
 		case html.TextToken:
 			t := tkn.Token()
 
-			if (strings.HasPrefix(t.Data, currentOccupancyText)) {
+			if strings.HasPrefix(t.Data, currentOccupancyText) {
 				occupancyTextReached = true
-			} else if (occupancyTextReached && isSpan) {				
+			} else if occupancyTextReached && isSpan {				
 				trimmed := strings.TrimSpace(t.Data)
 				occupancy, err := strconv.Atoi(trimmed)
 
-				if (err != nil) {
+				if err != nil {
 					log.Printf("Can't convert %s to int. Skipping further processing\n", trimmed)
 					return -2
 				} else {
@@ -50,5 +55,34 @@ func extractCurrentNumberOfPersons(techlibIndex string) int {
 	}
 }
 
+func getTechlibIndex() (string, error) {
+	resp, err := http.Get(techlibEnIndex)
+	if err != nil {
+		log.Printf("GET %s failed\n", techlibEnIndex) 
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Reading of requested data failed!")
+		return "", err
+	}
+
+	return string(body), nil
+}
+
 func main() {
+	log.Println("Starting app")
+
+	techlibIndex, err := getTechlibIndex()
+	if err != nil {
+		log.Panic("Err")
+	}
+
+	currentOccupancy := extractCurrentNumberOfPersons(techlibIndex) 
+
+
+	fmt.Printf("Current -> %d\n", currentOccupancy)
 }
